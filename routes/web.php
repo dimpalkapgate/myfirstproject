ee14b6d742d89ba240aeb7130a66588534a971f8
dimpalkapgate <dimpalkapgate2000@gmail.com>
2021-11-19 10:04:46 +0530

    making multiple page

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    
 });
+Route::get('contact', function () {
+    return view('contact');
+    
+});
+Route::get('band', function () {
+    return view('band');
+    
+});
+Route::get('tour', function () {
+    return view('tour');
+    
+});
+
+
